403d95c9e334fb250a50b6259d85c17cc7b56836
Anna Petrova <ann.undine@gmail.com>
2025-05-13 18:48:54 +0200

    add handleClick to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,18 @@
 import type { CardProps } from "../types/CardProps";
 
 export default function Card({ id, title, src, rating }: CardProps) {
+  const handleClick = (e) => {
+    const cardId = e.currentTarget.dataset.id;
+    console.log("Card ID:", cardId);
+    console.log("Props:", { id, title, src, rating });
+  };
   return (
-    <div className="card" data-id={id}>
-      <img src={src} alt={title} />
-      <h3>{title}</h3>
-      <p>Rating: {rating}</p>
-    </div>
+    <>
+      <div className="card" data-id={id} onClick={handleClick}>
+        <img src={src} alt={title} />
+        <h3>{title}</h3>
+        <p>Rating: {rating}</p>
+      </div>
+    </>
   );
 }
