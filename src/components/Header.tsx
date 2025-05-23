7b6144696507dfc38528d39e3b17c6f26554bdb7
Anna Petrova <ann.undine@gmail.com>
2025-05-23 15:48:09 +0200

    change logo path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,18 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
     <div className="headerContainer">
       <NavLink to="/">
-        <img src="./src/assets/logo.png" alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" />
       </NavLink>
-      
+
       <nav>
-        <NavLink to="/" >
-          Home
-        </NavLink>
-        <NavLink to="/movies" >
-          Movies
-        </NavLink>
-        <NavLink to="/series" >
-          Series
-        </NavLink>
-        <NavLink to="/reviews" >
-          Reviews
-        </NavLink>
-        
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/series">Series</NavLink>
+        <NavLink to="/reviews">Reviews</NavLink>
       </nav>
     </div>
   );
